49ce15a363c3b7bf563155026c6c96ca8cc3c7bb
ghaity <ghaity.mohamed@gmail.com>
2018-12-18 18:16:44 +0000

    get dada for authentication

--- src/app/service/registration.service.ts
+++ src/app/service/registration.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Utilisateur } from "app/models/user/utilisateur/utilisateur.model";
-import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireList, AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { HttpClient  } from "@angular/common/http";
 
 
@@ -8,6 +8,9 @@ import { HttpClient  } from "@angular/common/http";
 export class RegistrationService{
     private dbPath = 'utilisateur-db';
     utilisateurRef: AngularFireList<Utilisateur> = null;
+    restRef:AngularFireList<Utilisateur>;
+   
+
     constructor(public http: HttpClient,public db: AngularFireDatabase){
       this.utilisateurRef = this.db.list(this.dbPath);
     }
